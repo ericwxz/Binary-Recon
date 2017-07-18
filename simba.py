69ca6f0d2b3addc75baf80fc6d7a77eeb16b6737
root <root@localhost.localdomain>
2017-07-18 14:56:08 -0700

    Fixed SEAP

--- simba.py
+++ simba.py
@@ -3,7 +3,7 @@
 #Authors: Elijah Sheets & Eric Zhang
 #Project Start:7/17/2017
 #Last Edit:7/19/2017
-#Goal: Through collaboration with a fellow NREIP intern build a Binary Reconnaissance Tool to run specific scans and or analysis.
+#Goal: Through collaboration with a fellow SEAP intern build a Binary Reconnaissance Tool to run specific scans and or analysis.
 
 #imports
 import os
